f62479bb3b4bd95ca91b240f0dc9897fb50f3520
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-09-23 20:59:04 +0530

    Added: JavaScript demo files

---
+++ Demo/demo.js
@@ -0,0 +1,19 @@
+var val = 1;
+let dataType = typeof val;
+
+var val1 = "wdevw";
+let dataType1 = typeof val1;
+
+var val2 = true;
+let dataType2 = typeof val2;
+
+var val3 = typeof Math;
+let dataType3 = typeof val3;
+
+var val4 = null;
+let dataType4 = typeof val4;
+
+let sayHi = function () {
+  sayHi = console.log("Say Hi!!");
+};
+let dataType5 = typeof sayHi;
---
+++ Demo/if-else.js
@@ -0,0 +1,8 @@
+var age = 16;
+var status;
+if (age >= 14 && age < 19) {
+  status = "Eligible";
+} else {
+  status = "Not Eligible";
+}
+console.log(status);
